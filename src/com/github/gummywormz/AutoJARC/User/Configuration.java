221ae761562f4e980e355526d6afd9b04de2a8d7
gummywormz <hoopsandyoyofan26@live.com>
2014-10-04 14:28:42 -0500

    Add configuration class
    Class for representing the stuff found in AutoJarc.conf

---
+++ src/com/github/gummywormz/AutoJARC/User/Configuration.java
@@ -0,0 +1,96 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Paul Alves.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.gummywormz.AutoJARC.User;
+
+import java.io.File;
+
+/**
+ * Objective Representation of AutoJarc.conf 
+ * @author Paul Alves
+ */
+public class Configuration {
+    //also remember to add MAC OS stuffs (you should probably handle it on the config screen automatically rather than here though....)
+    private String workspaceDir;
+    private String chromePath;
+    
+    /**
+     * Creates a new configuration object
+     * @param ws Directory to the user's workspace
+     * @param cp Path to the Chrome executable (including the file itself)
+     */
+    public Configuration(String ws, String cp){
+    workspaceDir = ws;
+    chromePath = cp;
+    }
+    
+    /**
+     * Sets Chrome's path (including the executable file)
+     * @param path Path to Chrome
+     */
+    public void setChromePath(String path){
+    chromePath = path;
+    }
+    
+    /**
+     * Sets the workspace directory
+     * @param path Workspace directory path
+     */
+    public void setWorkSpace(String path){
+    workspaceDir = path;
+    }
+    
+    /**
+     * Returns this workspace as a String
+     * @return this workspace as a String
+     */
+    public String getWorkSpace(){
+    return workspaceDir;
+    }
+    
+    /**
+     * Returns the Chrome path as a String
+     * @return the Chrome path as a String
+     */
+    public String getChromePath(){
+    return chromePath;
+    }
+    
+    /**
+     * Returns the workspace directory as a File
+     * @return the workspace directory as a File
+     */
+    public File getWorkSpaceAsFile(){
+    return new File(workspaceDir);
+    }
+    
+    /**
+     * Returns the Chrome path as a File
+     * @return the Chrome path as a File
+     */
+    public File getChromePathAsFile(){
+    return new File(chromePath);
+    }
+    
+}
